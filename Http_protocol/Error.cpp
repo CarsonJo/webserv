5b85fd66f9e51d46fca2a68345b7ef2c5e770fea
Sebastien WITT <sebi12@hotmail.fr>
2024-11-26 20:00:47 +0100

    added error

--- Http_protocol/Error.cpp
+++ Http_protocol/Error.cpp
@@ -1,8 +1,12 @@
 #include "Error.hpp"
 
-std::string	e405(const VirtualServ* serv);
-std::string	e404(const VirtualServ* serv);
-std::string	e403(const VirtualServ* serv);
+std::string	e406(VirtualServ* serv);
+std::string	e405(VirtualServ* serv);
+std::string	e404(VirtualServ* serv);
+std::string	e403(VirtualServ* serv);
+std::string	e413(VirtualServ* serv);
+std::string	e500(VirtualServ* serv);
+std::string	e400(VirtualServ* serv);
 
 std::map<int, Error_function> fill_function()
 {
@@ -11,6 +15,10 @@ std::map<int, Error_function> fill_function()
 	ret[405] = e405;
 	ret[403] = e403;
 	ret[404] = e404;
+	ret[406] = e406;
+	ret[413] = e413;
+    ret[500] = e500;
+	ret[400] = e400;
 	return (ret);
 }
 
@@ -37,6 +45,39 @@ std::string	e405(const VirtualServ* serv)
 	return (ret);
 }
 
+// Mauvaise synthaxe ou mauvais param
+std::string e400(const VirtualServ* serv) {
+    std::string ret;
+    (void)serv;
+    ret.append(" Bad Request\r\n").append(Date().get_date());
+    return ret;
+}
+
+// Fichier uploade = trop lourd
+std::string e413(const VirtualServ* serv) {
+    std::string ret;
+    (void)serv;
+    ret.append(" Payload Too Large\r\n");// placeholder
+    return ret;
+}
+
+//Prob CGI, config cote client + tt le reste des exceptions non gerees avant
+std::string e500(const VirtualServ* serv) {
+    std::string ret;
+    (void)serv;
+    ret.append(" Internal Server Error\r\n").append(Date().get_date());
+    return ret;
+}
+
+std::string	e406(const VirtualServ* serv)
+{
+	std::string	ret;
+
+	(void)serv;
+	ret.append(" NOT Acceptable\r\n").append(Date().get_date());
+	return (ret);
+}
+
 std::string	e403(const VirtualServ* serv)
 {
 	std::string	ret;
@@ -49,8 +90,18 @@ std::string	e403(const VirtualServ* serv)
 std::string	e404(const VirtualServ* serv)
 {
 	std::string		ret;
-
+	// std::ifstream	f;
+	// char			buff[S404];
+	// int a;
 	(void)serv;
-	ret.append(" Not Found\r\n");
+	// f.open(ROOT, std::fstream::in);
+	// if (!f.is_open())
+	// 	std::cout << "asdfasdf" << std::endl;
+	// f.read(&buff[0], S404);
+	// a = f.gcount();
+	// std::cout << a << buff << std::endl;
+	ret.append(" Not Found\r\n");//.append("Content-Length: ").append(SIZE_404).append("\r\nContent-Type: text/html\r\n\r\n")
+	//.append(std::string(buff));
+
 	return (ret);
 }
