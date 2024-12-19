09217aedc472fea424b5fb13bd77148e428bf5a6
Abdelmajid Messaoud Nacer <abdmessa@bess-f2r4s10.clusters.42paris.fr>
2024-12-19 01:22:38 +0100

    ctrl C added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include "main.hpp"
 
+int g_signum = 0;
+
+void handle_sigint(int sig, siginfo_t *info, void *context) {
+    (void)info;
+    (void)context;
+
+    if(sig)
+    {
+        std::cerr << "\n Signal reçu. Arrêt du serveur..." << std::endl;
+        g_signum = 1;
+    }
+}
+
+
 int main(int argc, char **argv)
 {
 	Webserv		poll_test;
@@ -23,6 +37,18 @@ int main(int argc, char **argv)
 		return (0);
 	}
 	signal(SIGPIPE, SIG_IGN);
+	signal(SIGPIPE, SIG_IGN);
+	struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = handle_sigint; 
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        if (SIGINT)
+        {
+            herror("sigaction");
+            return 0;
+        }
+    }
 	try
 	{
 		while (1)
@@ -32,6 +58,9 @@ int main(int argc, char **argv)
 				poll_test.new_connect(err);
 				poll_test.handle_recv(err);
 			}
+			if (g_signum == 1) {
+        		break; 
+    		}
 		}
 	}
 	catch(const std::exception& e)
